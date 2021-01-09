573ced60897c0870ca7032a4e72483e517d4d6a7
sinnisingla@outlook.com <sinnisingla@outlook.com>
2021-01-09 16:30:11 +0530

    Pass search value to app component every time user enters using event emitter

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
     <div>
         <h1>Hello, World!</h1>
-        <SearchBar></SearchBar>
+        <SearchBar @newSearch = "onNewSearch"></SearchBar>
     </div>
 </template>
 
 <script>
     import SearchBar from './component/SearchBar';
+    const api_key = 'AIzaSyDQPsUczUrmUkBqdHLFWnorH-5LtgrOdg4';
 
     export default{
         name: "App",
-        components: {SearchBar} 
+        components: {SearchBar},
+        methods: {
+            onNewSearch: (newTerm) => {
+                console.log(newTerm);
+            }
+        }
     };
 </script>
