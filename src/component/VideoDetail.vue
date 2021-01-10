a62d07f3a93897bf6a13227a4fb1740533bfc411
sinnisingla@outlook.com <sinnisingla@outlook.com>
2021-01-11 00:24:12 +0530

    Embed video

--- src/component/VideoDetail.vue
+++ src/component/VideoDetail.vue
@@ -1,12 +1,35 @@
 <template>
-    <div>
-        {{selectedVideo.snippet.title}}
+    <div class="detail">
+        <div>
+            <iframe class="iframe" :src="videoUrl"></iframe>
+        </div>
+        <div>
+            <h4>{{selectedVideo.snippet.title}}</h4>
+        </div>
+        <p>{{selectedVideo.snippet.description}}</p>
     </div>
 </template>
 
 <script>
 export default {
     name: 'VideoDetail',
-    props: ['selectedVideo']
+    props: ['selectedVideo'],
+    computed: {
+        videoUrl: function() {
+            return `https://www.youtube.com/embed/${this.selectedVideo.id.videoId}`;
+        }
+    }
 }
 </script>
+
+<style scoped>
+.detail {
+    padding: 2% 5%;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+}
+.iframe {
+    width: 100%;
+    height: 500px;
+}
+</style>
